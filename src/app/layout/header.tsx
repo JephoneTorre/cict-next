8824efe8a824c530f19cf44dd61cb2c4db517e1e
joshuavillamora <earikjoshua@gmail.com>
2026-02-06 14:53:03 +0800

    feat: Add hamburger menu functionality

--- src/app/layout/header.tsx
+++ src/app/layout/header.tsx
@@ -3,21 +3,28 @@
 import Link from "next/link";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 const active =
-  "relative text-[var(--color-orange-light)] after:absolute after:left-[-7px] after:right-[-7px] after:-bottom-1 after:h-[3px] after:bg-[var(--color-orange-light)] after:rounded-full";
+  "relative text-white md:text-[var(--color-orange-light)] after:absolute after:left-[-7px] after:right-[-7px] after:-bottom-1 after:h-[3px] md:after:bg-[var(--color-orange-light)] after:rounded-full";
 const inactive = "";
 
 export default function Header({ inter }) {
   const pathname = usePathname();
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <header 
-      className={`${inter.className} flex md:grid md:grid-cols-3 items-center px-6 py-4 text-lg font-medium bg-orange-dark md:bg-transparent`}
+      className={`${inter.className} relative flex md:grid md:grid-cols-3 items-center px-6 py-4 text-lg font-medium bg-orange-dark md:bg-transparent`}
     >      
-      <button className="md:hidden absolute flex flex-col justify-center items-center w-6 h-8 space-y-1 col-start-3 justify-self-end">
+      {/* Hamburger icon */}
+      <button
+        onClick={() => setIsOpen(!isOpen)}
+        className="md:hidden absolute flex left-6 justify-center items-center w-6 h-8 space-y-1"
+      >
         <Image src="/hamburger-icon.png" width={20} height={15} alt="HAMBURGER" />
       </button>
+
       <Link 
         href="/" 
         className="flex justify-center items-center gap-x-2 col-span-3 md:col-span-1 mx-auto"
@@ -49,6 +56,42 @@ export default function Header({ inter }) {
           CONTACT
         </Link>
       </div>
+      
+
+      {/* Mobile menu */}
+      {isOpen && (
+        <div className={`${inter.className} absolute top-full left-0 w-full bg-orange-dark flex flex-col items-center gap-y-4 py-4 md:hidden z-50"`}>
+          <Link
+            href="/programs"
+            className={pathname === "/programs" ? active : inactive}
+            onClick={() => setIsOpen(false)}
+          >
+            PROGRAMS
+          </Link>
+          <Link
+            href="/news-announcements"
+            className={pathname === "/news-announcements" ? active : inactive}
+            onClick={() => setIsOpen(false)}
+          >
+            NEWS
+          </Link>
+          <Link
+            href="/faculty-profiles"
+            className={pathname === "/faculty-profiles" ? active : inactive}
+            onClick={() => setIsOpen(false)}
+          >
+            FACULTY
+          </Link>
+          <Link
+            href="/contact"
+            className={pathname === "/contact" ? active : inactive}
+            onClick={() => setIsOpen(false)}
+          >
+            CONTACT
+          </Link>
+        </div>
+      )}
+
     </header>
   );
 }
